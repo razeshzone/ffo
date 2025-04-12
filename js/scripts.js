5cfafa7617ecb7f06756194a9daf0b014a6778ec
Rajesh Kr. Das <razeshzone@gmail.com>
2025-04-12 14:54:38 +0545

    ffo page done
    all the parameters met and is done

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(function () {
+  // Init matchHeight
+  $('.matchHeight').matchHeight();
+
+  const $form = $('form');
+
+  $form.parsley({
+    excluded: 'textarea, :hidden, :disabled',
+    errorsWrapper: '<div class="invalid-feedback"></div>',
+    errorTemplate: '<div></div>',
+    trigger: 'change focusout'
+  });
+
+  $form.parsley().on('field:validated', function (fieldInstance) {
+    const $field = $(fieldInstance.$element);
+    if (fieldInstance.isValid()) {
+      $field.removeClass('is-invalid').addClass('is-valid');
+    } else {
+      $field.removeClass('is-valid').addClass('is-invalid');
+    }
+  });
+
+  $form.on('submit', function (e) {
+    e.preventDefault();
+  
+    if ($form.parsley().isValid()) {
+      $('#formSuccess').removeClass('d-none').addClass('show');
+  
+      // Scroll to message (optional)
+      $('html, body').animate({ scrollTop: 0 }, 'fast');
+  
+      // Reset form, Parsley, and field styles
+      $form[0].reset();
+      $form.parsley().reset();
+      $form.find('.form-control').removeClass('is-valid is-invalid');
+    }
+  });
+  
+
+});
